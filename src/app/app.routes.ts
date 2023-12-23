477089e10ba6e319881398a300b1ff0d95186b7e
otobit-shreya <arshreya@otobit.in>
2023-12-23 17:25:31 +0530

    Added Routing, components setup and packages installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+ 
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
