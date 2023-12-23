75f7d78c78f965fb91e61f638215f0d023d17612
otobit-shreya <arshreya@otobit.in>
2023-12-23 17:28:53 +0530

    main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: MainComponent },
   { path: 'signup', component: SignupComponent },
-
- 
+  { path: 'login', component: SignupComponent },
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
