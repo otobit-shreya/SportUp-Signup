6f2ff547e7a4259aa37087e3ce6669099bdb897c
otobit-shreya <arshreya@otobit.in>
2023-12-25 13:21:21 +0530

    home and verify component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { MainComponent } from './main/main.component';
 import { EnterdetailComponent } from './enterdetail/enterdetail.component';
+import { HomeComponent } from './home/home.component';
+import { VerifyComponent } from './verify/verify.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'enterdetail', component: EnterdetailComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'verify', component: VerifyComponent },
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
